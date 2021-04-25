c9082c9c540af0c91ee59cc270b777a3c95bec1d
Sebastian Jensen <s.nesgaard@gmail.com>
2021-04-25 20:28:08 +0200

    Added a bit of trolling

--- src/systems/map.lua
+++ src/systems/map.lua
@@ -179,11 +179,12 @@ local function get_map(x, y)
 
     if y < 500 then
         return "art/maps/build/blank.lua"
+    elseif y > 2000 and love.math.random(100) < 5 then
+        return "art/maps/build/end.lua"
     else
         rng = rng or love.math.newRandomGenerator(love.timer.getTime())
         --local rng = love.math.newRandomGenerator( y )
         local index = rng:random(#maps)
-        --index = 5
         local path = maps[index]
         return string.format("art/maps/build/%s", path)
     end
